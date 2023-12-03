6a2a0498577a3141ca61ea2564059509e938713d
Emelyanov535 <nouctanch777093@gmail.com>
2023-12-03 19:18:15 +0400

    Feature: SneakerService

--- src/main/java/com/android/android/Service/SneakerService.java
+++ src/main/java/com/android/android/Service/SneakerService.java
@@ -2,6 +2,8 @@ package com.android.android.Service;
 
 import com.android.android.Model.Sneaker;
 import com.android.android.Repository.SneakerRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -17,5 +19,33 @@ public class SneakerService {
     public Sneaker insert(String brand, String model, String description, Double price, Integer photo) {
         Sneaker sneaker = new Sneaker(brand, model, description, price, photo);
         return sneakerRepository.save(sneaker);
+    }
+
+    @Transactional
+    public Sneaker update(Long id, String brand, String model, String description, Double price, Integer photo){
+        final Sneaker sneaker = findSneaker(id);
+        sneaker.setBrand(brand);
+        sneaker.setModel(model);
+        sneaker.setDescription(description);
+        sneaker.setPrice(price);
+        sneaker.setPhoto(photo);
+        return sneakerRepository.save(sneaker);
+    }
+
+    @Transactional
+    public Sneaker delete(Long id){
+        final Sneaker curSneaker = findSneaker(id);
+        sneakerRepository.delete(curSneaker);
+        return curSneaker;
+    }
+
+    @Transactional
+    public Sneaker findSneaker(Long id){
+        return sneakerRepository.getReferenceById(id);
+    }
+
+    @Transactional
+    public Page<Sneaker> getAllSneakerPaged(int page, int size){
+        return sneakerRepository.findAll(PageRequest.of(page - 1, size));
     }
 }
