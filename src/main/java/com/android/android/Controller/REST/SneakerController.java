99aaaa6f267824f527e2b53938b0d12cc3455149
Emelyanov535 <nouctanch777093@gmail.com>
2023-12-03 19:34:48 +0400

    Feature: SneakerController

--- src/main/java/com/android/android/Controller/REST/SneakerController.java
+++ src/main/java/com/android/android/Controller/REST/SneakerController.java
@@ -1,11 +1,10 @@
 package com.android.android.Controller.REST;
 
 import com.android.android.Controller.DTO.SneakerDTO;
+import com.android.android.Model.Sneaker;
 import com.android.android.Service.SneakerService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping({"api/sneaker"})
@@ -18,6 +17,28 @@ public class SneakerController {
 
     @PostMapping({"/create"})
     public SneakerDTO create(@RequestBody SneakerDTO sneakerDTO) {
-        return new SneakerDTO(this.sneakerService.insert(sneakerDTO.getBrand(), sneakerDTO.getModel(), sneakerDTO.getDescription(), sneakerDTO.getPrice(), sneakerDTO.getPhoto()));
+        return new SneakerDTO(sneakerService.insert(sneakerDTO.getBrand(), sneakerDTO.getModel(), sneakerDTO.getDescription(), sneakerDTO.getPrice(), sneakerDTO.getPhoto()));
+    }
+
+    @PutMapping("/update/{id}")
+    public SneakerDTO update(@PathVariable Long id, @RequestBody SneakerDTO sneakerDTO){
+        return new SneakerDTO(sneakerService.update(id, sneakerDTO.getBrand(), sneakerDTO.getModel(), sneakerDTO.getDescription(), sneakerDTO.getPrice(), sneakerDTO.getPhoto()));
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public SneakerDTO delete(@PathVariable Long id){
+        return new SneakerDTO(sneakerService.delete(id));
+    }
+
+    @GetMapping("/get/{id}")
+    public SneakerDTO get(@PathVariable Long id){
+        return new SneakerDTO(sneakerService.findSneaker(id));
+    }
+
+    @GetMapping("/getAll")
+    public Page<SneakerDTO> getAll(@RequestParam(defaultValue = "1") int page,
+                                   @RequestParam(defaultValue = "5") int size) {
+        return sneakerService.getAllSneakerPaged(page, size)
+                .map(sneaker -> new SneakerDTO(new Sneaker(sneaker.getBrand(), sneaker.getModel(), sneaker.getDescription(), sneaker.getPrice(), sneaker.getPhoto())));
     }
 }
