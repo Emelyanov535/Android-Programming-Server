b0a24191062aaded66822c59246d112ed3a778d0
Emelyanov535 <nouctanch777093@gmail.com>
2023-12-06 23:44:00 +0400

    Feature: User realization

--- src/main/java/com/android/android/Controller/REST/UserController.java
+++ src/main/java/com/android/android/Controller/REST/UserController.java
@@ -1,9 +1,37 @@
 package com.android.android.Controller.REST;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.android.android.Controller.DTO.OrderDTO;
+import com.android.android.Controller.DTO.SignInDTO;
+import com.android.android.Controller.DTO.UserDTO;
+import com.android.android.Model.Order;
+import com.android.android.Service.UserService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping({"api/user"})
+@RequestMapping("api/user")
 public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/signin")
+    public UserDTO signIn(@RequestBody SignInDTO signInDTO){
+        return new UserDTO(userService.signIn(signInDTO.getEmail(), signInDTO.getPassword()));
+    }
+
+    @PostMapping("/signup")
+    public UserDTO signUp(@RequestBody UserDTO userDTO){
+        return new UserDTO(userService.signUp(userDTO));
+    }
+
+    @GetMapping("/getorders/{id}")
+    public List<OrderDTO> getUserOrders(@PathVariable("id") Long id){
+        return userService.getUserOrders(id).stream()
+                .map(OrderDTO::new)
+                .toList();
+    }
 }
---
+++ src/main/java/com/android/android/Service/UserService.java
@@ -0,0 +1,48 @@
+package com.android.android.Service;
+
+import com.android.android.Controller.DTO.UserDTO;
+import com.android.android.Model.Basket;
+import com.android.android.Model.Order;
+import com.android.android.Model.User;
+import com.android.android.Repository.UserRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Objects;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Transactional
+    public User signIn(String email, String password){
+        User user = userRepository.findByEmail(email);
+        if(Objects.equals(user.getPassword(), password)){
+            return user;
+        }
+        return null;
+    }
+
+    @Transactional
+    public User signUp(UserDTO user){
+        User userCreate = new User(user.getUsername(), user.getEmail(), user.getPassword(), user.getPhoto(), user.getRole());
+        return userRepository.save(userCreate);
+    }
+
+    @Transactional
+    public List<Order> getUserOrders(Long id){
+        User user = userRepository.getReferenceById(id);
+        return user.getOrders();
+    }
+
+    @Transactional
+    public Basket getUserBasket(Long id){
+        User user = userRepository.getReferenceById(id);
+        return user.getBasket();
+    }
+}
